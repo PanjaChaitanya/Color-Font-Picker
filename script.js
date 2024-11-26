d6d5dab9696705dc39bd96da59a9383bc257c256
Panja Chaitanya <155937982+PanjaChaitanya@users.noreply.github.com>
2024-11-26 12:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let bckColor=(clr)=>{document.querySelector('.textContainer').style.backgroundColor=clr}
+let textColor=(clr)=>{document.querySelector('.textContainer').style.color=clr}
+let textFontSize=(size)=>{document.querySelector('.textContainer').style.fontSize=size+'px'}
+let textFontFamily=(fontName)=>{document.querySelector('.textContainer').style.fontFamily=fontName}
